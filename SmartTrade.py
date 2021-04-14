90641b4f7ec7a78b9cee6f6b94241e0e8242033a
Novixel <71154930+OfficialNovixel@users.noreply.github.com>
2021-04-14 02:00:24 -0700

    Add files via upload

---
+++ SmartTrade.py
@@ -0,0 +1,94 @@
+# smart trade by novixel
+
+# SmarTrade.py
+# import as Smt 
+import ConfigSetup as cfg
+from Connect import CoinConnect as CC
+
+LEFTA, RIGHTA = cfg.product_id.split("-")
+lasttrademade = 0.0
+
+Coin = CC()
+auth = Coin.auth
+
+def getLastTrade():
+    filled = auth.get_fills(cfg.product_id)
+    for i in filled:
+        lastside = i["side"]
+        lastprice = i['price']
+        break
+    return lastside, float(lastprice)
+
+def updateFunds():
+        funds = auth.get_accounts()
+        x = -1
+        for i in funds:
+            x += 1
+            for k,v in i.items():
+                if v == LEFTA:
+                    lefta = funds[x]
+                    for b,t in lefta.items():
+                        cfg.SaveLeftAccount(str(b),str(t))
+                elif v == RIGHTA:
+                    righta = funds[x]
+                    for u,d in righta.items():
+                        cfg.SaveRightAccount(str(u),str(d))
+
+def updateTick():
+        tick = auth.get_product_ticker(cfg.product_id)
+        for k,v in tick.items():
+            cfg.SaveTicker(str(k), str(v))
+        return float(tick["price"])
+
+def update24Hour():
+        day = auth.get_product_24hr_stats(cfg.product_id)
+        for k,v in day.items():
+            cfg.SaveDay(str(k), str(v))
+        
+
+def makeTrade(product_id, side, price, size):
+    """Final Stop Where the trade is made with SIDE,SIZE,AMOUNT"""
+    global trade
+    if cfg.ReadLASTTRADE("size") != str(size):
+        trade = auth.place_order(
+            product_id= product_id,
+            side= side, 
+            order_type= 'limit',
+            price= price , 
+            size= size )
+        print(trade)
+        for k,v in trade.items():
+            cfg.SaveTrade(str(k), str(v))
+    else:
+        print("Market Hasnt Moved Enough Since Our Last Trade")
+
+
+def MarketCheck(): #### THIS DECIDES IF WE EVEN ATTEMPT a trade
+    # Is it a good time to trade in this market?
+    print("##Checking Market##\nCurrentPrice:", cfg.ReadTICKER("price"))
+    print("\n24HOUR last:", cfg.ReadDAYSTATS("last"))
+    c = float(cfg.ReadTICKER("price"))   #current price
+    h = float(cfg.ReadDAYSTATS("high"))   #current 24hr high
+    l = float(cfg.ReadDAYSTATS("low") )  #current 24hr low
+    b = ((h + l) / 2) # 24hourBaseLine
+    ba = (b / 100) # 1% of that baseline
+    bu = ba + b # add 1% of the base to counter sell fee
+    bd = b - ba # Minus 1% of the base to counter buy fee
+    f = ((b + c) / 2) # BaseLine of price and 24h base
+    _,l = getLastTrade()
+    ld = c - l # current price minus our last trade price
+    ldp = 100 * (ld / c)
+    print("\n\nChecking Market At\t", cfg.ReadTICKER("time"), "\n",
+        "\n\n\t\t24hourBaseline\t=\t",      "%.8f" % b,
+        "\n\n\t\tUpperBounds\t=\t",      "%.8f" % bu,
+        "\n\n\t\tLowerBounds\t=\t",      "%.8f" % bd,
+        "\n\n\tCurrent Market Price\t=\t",  "%.8f" % c,
+        "\n\n\tLast Trade Price\t=\t",  "%.8f" % l,
+        "\n\n\tDifference last/cur \t=\t",  "%.2f" % ldp, "%")
+    if bd <= c <= bu:
+        return False , bd , bu
+    else:
+        return True, bd , bu
+    
+
+    
---
+++ TheTrader.py
@@ -0,0 +1,170 @@
+import ConfigSetup as cfg
+import SmartTrade
+
+def checkFunds(base,quot): # lets check our balance
+    """ BaseAvailable , QuoteAvailable, will return bool int
+    bool = true if we have enough, 
+    int = what we can trade if any,
+    3 is none
+    """
+    minTrdsize = 0.000100
+    minFunamont = 10
+    if base >= minTrdsize and quot >= minFunamont: 
+        return True, 0 # we have enough funds in both
+    elif quot >= minFunamont:
+        return True, 1 # we have enough to only buy
+    elif base >= minTrdsize:
+        return True, 2 # only enough to sell
+    else:
+        return False, 3 # we have no funds
+
+#Step #2 - Trader Asks data if we are able to trade
+def CanWeTrade(current_price, lastTradePrice, lastTradeSide, base_Avail, quote_Avail):
+    # Check Funding Calculations
+    tradeFunds, whatTrade = checkFunds(base_Avail,quote_Avail)
+
+    # DEBUG INFO
+    print("\n#CANWETRADE##DEBUG###\n\tChecking of funds:",tradeFunds, whatTrade)
+    print("\n#CANWETRADE#DEBUG#END###\n")
+
+    # Calculation of if,and,or,but
+    if lastTradePrice != current_price: 
+        if (tradeFunds) and (whatTrade == 0):
+            canTrade = True
+            canBuy = True
+            canSell = True
+            return 'both'
+        elif (tradeFunds) and (whatTrade == 1):
+            canTrade = True
+            canBuy = True
+            canSell = False
+            return 'buy'
+        elif (tradeFunds) and (whatTrade == 2):
+            canTrade = True
+            canBuy = False
+            canSell = True
+            return 'sell'
+        else:
+            canTrade = False
+            canBuy = False
+            canSell = False
+            return 'none'
+    else:
+        print(lastTradePrice," lastTradePrice is the same as current_price", current_price)
+        return 'none'
+        
+
+# Step #1 - Ask The Trader If We Can Trade!
+def ShouldWeTrade():
+    # Get Last Trade We Made!
+    lastTradeSide, lastTradePrice = SmartTrade.getLastTrade()
+
+    # Update Our Currency Accounts
+    SmartTrade.updateFunds()
+
+    # base_Ext = "BTC"/"EUR" = quote_ext
+    base_ext = cfg.ReadLEFTaccount("currency")
+    quot_ext = cfg.ReadRIGHTaccount("currency")
+
+    # Get Available Balances
+    base_Avail = float(cfg.ReadLEFTaccount("available"))
+    quote_Avail = float(cfg.ReadRIGHTaccount("available"))
+
+    # Get Current Market Price As of last Tick (*Most Accurate Recent Price)
+    current_price = SmartTrade.updateTick()
+    
+    # How Much of quote does it cost to buy 1 of the base
+    exchange_rate = 1 / current_price
+    # 
+    current_base_total = (exchange_rate * quote_Avail) + base_Avail
+    current_quot_total = current_price * current_base_total
+
+    # Finally The Trader Uses This Data To Ask if We CAN trade
+    whatTrade = CanWeTrade( current_price, 
+                            lastTradePrice, 
+                            lastTradeSide,
+                            base_Avail,
+                            quote_Avail)
+
+    # DEBUG INFO #
+    print("\n#SHOULDWETRADE#DEBUG INFO###\n")
+    print("\nOur last trade was a",lastTradeSide,"at the price of",lastTradePrice)
+    print("\nbase avail:",base_ext,base_Avail)
+    print("quot avail:",quot_ext,quote_Avail)
+    print("\ntotal base:",current_base_total, base_ext)
+    print("total quote:",current_quot_total, quot_ext)
+    print("\nCurrent Market Price:",current_price)
+    print("\nWe can make a trade with side:",whatTrade)
+    print("\n#SHOULDWETRADE#DEBUG#END###\n")
+    ##############
+
+    #return with the trade we can do 
+    return whatTrade, current_quot_total, quot_ext, base_ext
+
+def CheckTheMarket():
+    print('\nTrader is Checking the market.')
+    SmartTrade.updateTick()
+    SmartTrade.update24Hour()
+    SmartTrade.updateFunds()
+    markCheck,lower,upper = SmartTrade.MarketCheck()
+    return markCheck , lower, upper
+
+def DecideOnTrade(lower,upper): #Big Function
+    print('\nTrader is Deciding on a Trade.')
+    cp = float(cfg.ReadTICKER("price"))
+    product_id = cfg.product_id
+    price = cp
+    if lower > cp: #### BUY Trade
+        diffDec = lower - cp # the decrease from the lower bound
+        percDec = diffDec / cp * 100
+        side = 'buy'
+        if percDec >= 5:
+            print("5% Below our lower bounds\n",percDec)
+            size = 0.0003
+            SmartTrade.makeTrade(product_id, side, price, size)
+        elif percDec >= 4:
+            print("4% Below our lower bounds\n",percDec)
+            size = 0.00025
+            SmartTrade.makeTrade(product_id, side, price, size)
+        elif percDec >= 3:
+            print("3% Below our lower bounds\n",percDec)
+            size = 0.0002
+            SmartTrade.makeTrade(product_id, side, price, size)
+        elif percDec >= 2:
+            print("2% Below our lower bounds\n",percDec)
+            size = 0.00015
+            SmartTrade.makeTrade(product_id, side, price, size)
+        elif percDec >= 1:
+            print("1% Below our lower bounds\n",percDec)
+            size = 0.0001
+            SmartTrade.makeTrade(product_id, side, price, size)
+        else:
+            print(percDec)
+    else: ### Sell Trade
+        diffInc = cp - upper # the increase from the upper limit
+        percInc = diffInc / cp * 100 #Percent of increse
+        side = 'sell'
+        if percInc >= 5:
+            print("5% Above our upper bounds\n", percInc)
+            size = 0.0003
+            SmartTrade.makeTrade(product_id, side, price, size)
+        elif percInc >= 4:
+            print("4% Above our upper bounds\n", percInc)
+            size = 0.00025
+            SmartTrade.makeTrade(product_id, side, price, size)
+        elif percInc >= 3:
+            print("3% Above our upper bounds\n", percInc)
+            size = 0.0002
+            SmartTrade.makeTrade(product_id, side, price, size)
+        elif percInc >= 2:
+            print("2% Above our upper bounds\n", percInc)
+            size = 0.00015
+            SmartTrade.makeTrade(product_id, side, price, size)
+        elif percInc >= 1:
+            print("1% Above our upper bounds\n", percInc)
+            size = 0.0001
+            SmartTrade.makeTrade(product_id, side, price, size)
+        else:
+            print(percInc)
+        # product_id, side, price, size
+        #SmartTrade.makeTrade()
